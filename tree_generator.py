085bc70669c0a86058ffc70a122ab0729f967c88
Paul Ngo <paulngo123@gmail.com>
2020-07-18 20:10:31 -0700

    update question nodes to contain their answers

--- tree_generator.py
+++ tree_generator.py
@@ -14,7 +14,7 @@ All nodes require all parents to have been completed in order to access them.
 if __name__ == "__main__":
     start_node = ["start", "start", "start", "Which category would you like to begin exploring?", 0, ["culture0"]]
     culture0_node = ["culture0", "culture", "resource", "https://www.amazon.jobs/en/principles", 0, ["culture1"]]
-    culture1_node = ["culture1", "culture", "question", "Which leadership principle involves starting with the customer and working backwards?", 100, ["end"]]
+    culture1_node = ["culture1", "culture", "question", ["Which leadership principle involves starting with the customer and working backwards?", "customer obsession"], 100, ["end"]]
     end_node = ["end", "end", "end", "Congratulations on finishing the course!", 0, []]
     all_nodes = [start_node, culture0_node, culture1_node, end_node]
     with open("tree.txt", "w") as f:
